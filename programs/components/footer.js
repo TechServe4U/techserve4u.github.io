8dcbfde95e5aae2e7865b96ca5c12d184294bef6
Moin <moinulislam.dev@gmail.com>
2021-03-06 20:59:07 +0600

    updated program 1

---
+++ programs/components/footer.js
@@ -0,0 +1,73 @@
+const footersData = [
+    {
+    }
+];
+
+
+
+function footerTemplate(footer) {
+    return `
+    <footer>
+        <div class="container">
+            <div class="row">
+                <div class="col-md-3">
+                    <div>
+                        <ul>
+                            <li>Programs</li>
+                            <li><a href="#">E2E API Test</a></li>
+                            <li><a href="#">QA API Automation Engineer</a></li>
+                            <li><a href="#">E2E Web and API Automation</a></li>
+                        </ul>
+                    </div>
+                </div>
+                <div class="col-md-3">
+                    <div>
+                        <ul>
+                            <li>Products</li>
+                            <li><a href="https://staging.biyekorun.us/">Biye Korun</a></li>
+                        </ul>
+                    </div>
+                </div>
+                <div class="col-md-3">
+                    <div>
+                        <ul>
+                            <li>Services</li>
+                            <li><a href="#">Application Development</a></li>
+                            <li><a href="#">Mobile Application Development</a></li>
+                            <li><a href="#">Web application development</a></li>
+                        </ul>
+                    </div>
+                </div>
+                <div class="col-md-3">
+                    <div>
+                        <ul>
+                            <li>Support</li>
+                            <li><a href="../help-and-faq.html">Help & FAQ</a></li>
+                            <li><a href="../privacy-policy.html">Privacy Policy</a></li>
+                            <li><a href="../terms-and-conditions.html">Terms & Conditions</a></li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+
+            <div class="row">
+                <div class="col-md-12">
+                    <p class="copyright">&copy; TechServe4u LLC 2013-2021</p>
+                    <ul class="social-icon text-center">
+                        <li><a href="https://www.facebook.com/techserve4u/"><i class="fab fa-facebook-square"></i></a></li>
+                        <li><a href="https://www.youtube.com/channel/UCQsoLRTB8n7iPBWI1noZTiw"><i class="fab fa-youtube-square"></i></a></li>
+                        <li><a href="https://www.linkedin.com/company/techserve4u/"><i class="fab fa-linkedin"></i></a></li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    </footer>
+    `;
+}
+
+
+document.getElementById("footer").innerHTML = `
+    ${footersData.map(footerTemplate).join("")}
+  `;
+
+
---
+++ programs/components/nav.js
@@ -0,0 +1,92 @@
+const navsData = [
+    {
+    }
+];
+
+
+
+function navTemplate(nav) {
+    return `
+    <div class="icon-bar">
+        <a href="https://wa.me/15868348526" target="_blank" class="twitter"><i class="fab fa-whatsapp"></i></a>
+        <a href="https://www.facebook.com/techserve4u/" target="_blank" class="facebook"><i
+                class="fab fa-facebook"></i></a>
+        <a href="https://www.youtube.com/channel/UCQsoLRTB8n7iPBWI1noZTiw" target="_blank" class="youtube"><i
+                class="fab fa-youtube"></i></a>
+        <a href="https://join.slack.com/t/techserve4ullc/shared_invite/zt-mik6lmar-mpTFcBIOi~uQiuYxfFsUBA"
+            target="_blank" class="google"><i class="fab fa-slack"></i></a>
+        <a href="https://www.linkedin.com/company/techserve4u/" target="_blank" class="linkedin"><i
+                class="fab fa-linkedin"></i></a>
+    </div>
+
+
+    <div class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel"
+        aria-hidden="true">
+        <div class="modal-dialog" role="document">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h5 class="modal-title" id="exampleModalLabel">Our Next Live</h5>
+                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                        <span aria-hidden="true">&times;</span>
+                    </button>
+                </div>
+                <div>
+                    <img class="popup" src="../../images/popup/next-live.jpg" alt="">
+                </div>
+                <div class="modal-footer">
+                    <a href="https://www.facebook.com/techserve4u/" target="_blank" class="btn btn-primary">Join
+                        us on Facebook</a>
+                    <a href="https://www.youtube.com/channel/UCQsoLRTB8n7iPBWI1noZTiw" target="_blank"
+                        class="btn btn-danger">Join us on YouTube</a>
+                </div>
+            </div>
+        </div>
+    </div>
+
+
+    <div class="container">
+        <div class="d-flex">
+            <div class="mr-auto p-2 navbar navbar-expand-lg navbar-expand-sm">
+                <ul class="navbar-nav mr-auto">
+                    <li class="nav-item"><a class="nav-link call" href="tel:+15868348526"><i
+                                class="nav-icon fas fa-phone-alt"></i>
+                            +(1)
+                            586-834-8526</a></li>
+                    <li class="nav-item"><a class="nav-link email" href="mailto:info@techserve4u.com"><i
+                                class="nav-icon fas fa-envelope"></i>
+                            info@techserve4u.com</a></li>
+                    <li class="nav-item"><a class="nav-link address"
+                            href="https://www.google.com/search?client=firefox-b-d&q=techserve4u" target="_blank"><i
+                                class="nav-icon fas fa-map-marker-alt"></i> Warren,
+                            MI</a></li>
+                </ul>
+            </div>
+            <div class="p-2 navbar navbar-expand-lg navbar-expand-sm">
+                <ul class="navbar-nav mr-auto">
+                    <li class="nav-item"><a class="nav-link" href="https://www.facebook.com/techserve4u/"
+                            target="_blank"><i class="nav-icon fab fa-facebook-square fb"></i></a>
+                    </li>
+                    <li class="nav-item"><a class="nav-link"
+                            href="https://www.youtube.com/channel/UCQsoLRTB8n7iPBWI1noZTiw" target="_blank"><i
+                                class="nav-icon fab fa-youtube-square yt"></i></a></li>
+                    <li class="nav-item"><a class="nav-link" href="https://www.linkedin.com/company/techserve4u/"
+                            target="_blank"><i class="nav-icon fab fa-linkedin ldi"></i></a>
+                    </li>
+                    <li class="nav-item"><a class="nav-link" href="https://join.slack.com/t/techserve4ullc/shared_invite/zt-mik6lmar-mpTFcBIOi~uQiuYxfFsUBA"
+                            target="_blank"><i class="nav-icon fab fa-slack sl"></i></a>
+                    </li>
+                    <li class="nav-item"><a class="nav-link live" href="#" data-toggle="modal"
+                            data-target="#exampleModal">&#9679; Live</a></li>
+                </ul>
+            </div>
+        </div>
+    </div>
+    `;
+}
+
+
+document.getElementById("nav").innerHTML = `
+    ${navsData.map(navTemplate).join("")}
+  `;
+
+
