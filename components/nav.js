bcb09b8c66273d0aeda8b6a782916c71dff5f063
Moin <moinulislam.dev@gmail.com>
2021-03-21 21:55:21 +0600

    added slide and live

--- components/nav.js
+++ components/nav.js
@@ -32,10 +32,10 @@ function navTemplate(nav) {
                     </button>
                 </div>
                 <div>
-                    <div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
+                    <div id="carouselExampleIndicators0" class="carousel slide" data-ride="carousel">
                         <ol class="carousel-indicators">
-                            <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
-                            <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
+                            <li data-target="#carouselExampleIndicators0" data-slide-to="0" class="active"></li>
+                            <li data-target="#carouselExampleIndicators0" data-slide-to="1"></li>
                         </ol>
                         <div class="carousel-inner">
                             <div class="carousel-item active">
@@ -45,11 +45,11 @@ function navTemplate(nav) {
                                 <img class="d-block w-100" src="images/live/next-live2.jpg" alt="Second slide">
                             </div>
                         </div>
-                        <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
+                        <a class="carousel-control-prev" href="#carouselExampleIndicators0" role="button" data-slide="prev">
                             <span class="carousel-control-prev-icon" aria-hidden="true"></span>
                             <span class="sr-only">Previous</span>
                         </a>
-                        <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
+                        <a class="carousel-control-next" href="#carouselExampleIndicators0" role="button" data-slide="next">
                             <span class="carousel-control-next-icon" aria-hidden="true"></span>
                             <span class="sr-only">Next</span>
                         </a>
